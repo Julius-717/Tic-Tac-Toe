de30adc6e4c225cca265ec277d45e7d628a2f0d9
Julius-717 <waynejulius10@gmail.com>
2019-11-30 05:47:15 +0300

    add a function that returns the number of moves played

--- script.js
+++ script.js
@@ -43,4 +43,16 @@ function Board (optons){
             turn: Math.round(Math.random()),
         }
     }
+
+    function moveCount(board){
+        let moveCount = 0
+        for (let i = 0; i<board.length; i++){
+            for (let j = 0 ; j<board[i].length ; j++){
+                if (board[i][j]!=""){
+                    moveCount++
+                }
+            }
+        }
+        return moveCount
+    }
 }
